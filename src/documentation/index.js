c092cc0e2f5f430ccbab0367ce4f6879fb220eb3
suave990 <kezasuave990@gmail.com>
2023-01-31 17:32:23 -0800

    setting up swagger
    setting up swagger
    
    installing swagger and morgan

--- src/documentation/index.js
+++ src/documentation/index.js
@@ -1,1 +1,52 @@
-// add swagger doc files here
+import { Router } from "express";
+import { serve, setup } from "swagger-ui-express";
+import dotenv from "dotenv";
+
+const docrouter = Router();
+dotenv.config();
+const host = process.env.SWAGGER_SERVER;
+const options = {
+  openapi: "3.0.1",
+  info: {
+    title: "TROJAN-EC-BN",
+    version: "1.0.0",
+    description: "atlp rwanda - Trojan team ",
+  },
+  servers: [
+    {
+      url: host,
+    },
+  ],
+  basePath: "/",
+  security: [
+    {
+      bearerAuth: [],
+    },
+  ],
+  tags: [],
+  paths: {
+    "/home": {
+      get: {
+        tags: ["homepage"],
+        description: "Welcome message",
+        parameters: [],
+        responses: {
+          200: {
+            description: "successfully",
+          },
+        },
+      },
+    },
+  },
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: "http",
+        scheme: "bearer",
+        bearerFormat: "JWT",
+      },
+    },
+  },
+};
+docrouter.use("/", serve, setup(options));
+export default docrouter;

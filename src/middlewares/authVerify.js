8e6ad733d2e1a64aee6e4f443f919d2e79d29931
suave990 <kezasuave990@gmail.com>
2023-02-09 12:19:05 -0800

    checking authentication through middlewares

---
+++ src/middlewares/authVerify.js
@@ -0,0 +1,6 @@
+import passport from "passport";
+
+const authLogin = passport.authenticate("local");
+const checkAuth=passport.authenticate("jwt",{session: false})
+
+export {authLogin, checkAuth};
